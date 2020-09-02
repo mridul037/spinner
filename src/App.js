7c6f7b383da942f3a4b1bdf4b986f8ebeb01050a
Quarantine Coder <mridulshukla037@gmail.com>
2020-09-02 22:28:15 +0530

    changes spinner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Circle from './Circle';
+import Card from './Card';
+export class App extends React.Component {
 
-function App() {
+  constructor() {
+    super();
+    this.places = ['Pizzas', 'Sandwiches', 'Salads', 'Soup', 'Japanese food', 'Pastas'];
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Circle items={this.places}/>
+      <div className="logo">
+      <img src={logo} alt="Logo" />
+      </div>
+      <Card></Card>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+export default function Card(){
+  return (
+    <div >
+      Spin the wheel now to
+             get rewarded
+    </div>
+  );
+};
